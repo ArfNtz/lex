b6dd46f08a1f726ed13328fe937d96a9799756e1
Frantz Epineuse <arfntz@gmail.com>
2020-02-25 09:44:51 +0100

    Update mathLex.swift

--- Sources/lex/mathLex.swift
+++ Sources/lex/mathLex.swift
@@ -2,20 +2,19 @@ import Foundation
 
 // langage elements
 public enum MathLexem {
-    case Define
+    case Keyword
     case Identifier(String)
     case Number(Float)
     case ParensOpen
     case ParensClose
     case Comma
-    case Other(String)
     case BinaryOp(String)
 }
 
 // dictionary
 public let math_dict: [Def<MathLexem>] = [
     Def<MathLexem>(regex: "[ \t\n]" , funct: { _ in nil } ),
-    Def<MathLexem>(regex: "[a-zA-Z][a-zA-Z0-9]*" , funct: { $0 == "def" ? .Define : .Identifier($0) } ),
+    Def<MathLexem>(regex: "[a-zA-Z][a-zA-Z0-9]*" , funct: { $0 == "key" ? .Keyword : .Identifier($0) } ),
     Def<MathLexem>(regex: "#[0-9.]+" , funct: {(r: String) in .Number((r as NSString).floatValue) } ),
     Def<MathLexem>(regex: "\\(" , funct: { _ in .ParensOpen } ),
     Def<MathLexem>(regex: "\\)" , funct: { _ in .ParensClose } ),
